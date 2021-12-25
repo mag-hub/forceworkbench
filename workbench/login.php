da674cea9fcb56d19862c19f4ac2727281f7d824
Abhishek Patil <patilabhishek168@gmail.com>
2021-12-25 18:31:00 +0530

    updated login page

--- workbench/login.php
+++ workbench/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once "shared.php";
 require_once "session.php";
-// require_once "controllers/LoginController.php";
+require_once "controllers/LoginController.php";
 
 $c = new LoginController();
 if (isset($_POST['uiLogin'])
